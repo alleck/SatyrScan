d66dd9b3d4377c0b9161b5966966a398f24b48f2
alleck <kaleb.lewis92@gmail.com>
2014-02-10 03:20:29 -0600

    Update systemscan.sh

--- SatyrScan-1.1.0/systemscan.sh
+++ SatyrScan-1.1.0/systemscan.sh
@@ -18,24 +18,36 @@ then
         rm sourcefiles/virloc.source
 fi
 
+ls -ld /home/*/public_html/ | awk '{print $9}' > dirs.source
+
 echo -e "\033[35mFinding Scripts... \033[0m"
 
 percent=$(wc -l virs.source | awk '{print $1}')
+percent2=$(wc -l dirs.source | awk '{print $1}')
 
 echo $percent
+echo $percent2
+percent3=$(( $percent * $percent2 ))
 
-process=$(bc -l <<< "scale = 2;  100 / $percent")
+echo $percent3
+
+process=$(bc -l <<< "scale = 3;  100 / $percent3")
 proc2=$process
+
+while read dirs
+do
 
 #Get script locations
 while read line
 do
         echo $process
-        grep -lE -R --exclude="virs.source" "$line" /home*/*/public_html/* >> sourcefiles/virloc.source
+        grep -lE -R --exclude="virs.source" "$line" $dirs  >> sourcefiles/virloc.source
         process=$(bc -l <<< "scale = 2; $process + $proc2")
 
 
 done < virs.source
+
+done < dirs.source
 
 #Sort out duplicates
 echo -e "\033[35mSorting... \033[0m"
@@ -63,3 +75,4 @@ read -p "Do you wish to REMOVE the infected files? " remove
         echo " "
         echo -e "\033[32mPlease check virus.local for file locations\033[0m"
         fi
+
