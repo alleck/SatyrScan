b14580ff57cc8ae8f3dbe623af9e8731ab97c76b
alleck <kaleb.lewis92@gmail.com>
2014-02-10 00:52:02 -0600

    added install file

---
+++ SatyrScan-1.1.0/install.sh
@@ -0,0 +1,8 @@
+mkdir /home/sscan
+cd /home/sscan
+wget https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.1.0/virs.source
+wget https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.1.0/systemscan.sh
+wget https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.1.0/mailscan.sh
+wget https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.1.0/removevirs.sh
+wget https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.1.0/removespam.sh
+wget https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.1.0/keyword.source
