95e1f1ee179485bcc21a5300dbfe1f139e8b836a
alleck <kaleb.lewis92@gmail.com>
2014-03-08 02:26:54 -0600

    Update install.sh

--- SatyrScan-1.2.0/install.sh
+++ SatyrScan-1.2.0/install.sh
@@ -1,19 +1,22 @@
 mkdir /home/sscan
 cd /home/sscan
-echo -ne '####                      (14%)\r'
+echo -ne '###                       (11%)\r'
 wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/virs.source
-echo -ne '#######                   (28%)\r'
+echo -ne '#####                     (22%)\r'
 wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/systemscan.sh
+echo -ne '#########                 (33%)\r'
 wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/accountscan.sh
+echo -ne '############              (44%)\r'
 wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/scan.sh
+echo -ne '##############            (55%)\r'
 wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/config.sh
-echo -ne '############              (42%)\r'
+echo -ne '################          (66%)\r'
 wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/mailscan.sh
-echo -ne '################          (57%)\r'
+echo -ne '###################       (77%)\r'
 wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/removevirs.sh
-echo -ne '###################       (71%)\r'
+echo -ne '######################    (88%)\r'
 wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/removespam.sh
-echo -ne '######################    (85%)\r'
+echo -ne '########################  (99%)\r'
 wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/keyword.source
 echo -ne '##########################(100%)\r'
 echo -ne '\n'
