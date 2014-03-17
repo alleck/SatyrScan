eb22cece1a8702da228c9037df786cd81595264d
alleck <alleck@alleck-OptiPlex-3010.(none)>
2014-03-16 23:48:15 -0500

    update script added

--- SatyrScan-1.2.0/install.sh
+++ SatyrScan-1.2.0/install.sh
@@ -13,6 +13,7 @@ wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/config.sh
 echo -ne '################          (66%)\r'
 wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/mailscan.sh --no-check-certificate
 echo -ne '###################       (77%)\r'
+wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/version.txt --no-check-certificate
 wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/removevirs.sh --no-check-certificate
 echo -ne '######################    (88%)\r'
 wget -q https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/removespam.sh --no-check-certificate
---
+++ SatyrScan-1.2.0/update.sh
@@ -0,0 +1,14 @@
+rm -f /home/sscan/accountscan.sh
+rm -f /home/sscan/config.sh
+rm -f /home/sscan/install.sh
+rm -f /home/sscan/keyword.source
+rm -f /home/sscan/mailscan.sh
+rm -f /home/sscan/README
+rm -f /home/sscan/removespam.sh
+rm -f /home/sscan/removevirs.sh
+rm -f /home/sscan/scan.sh
+rm -f /home/sscan/systemscan.sh
+rm -f /home/sscan/update.sh
+rm -f /home/sscan/version.txt
+rm -f /home/sscan/virs.source
+wget https://raw.github.com/alleck/SatyrScan/master/SatyrScan-1.2.0/install.sh --no-check-certificate && sh install.sh
